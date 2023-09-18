af33b596c4b4a64876b6d2ea27a99493f88cb38e
kareem hagag <kareem.m.hagag@gmail.com>
2023-09-18 15:14:25 +0300

    initial commit

---
+++ src/main/java/com/kareem/ormnativequery/OrmNativeQueryApplication.java
@@ -0,0 +1,13 @@
+package com.kareem.ormnativequery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmNativeQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrmNativeQueryApplication.class, args);
+	}
+
+}
